07a197db7a6cf455b5fec57de00736f72d0fb18d
SnowmanNunu <345750542@qq.com>
2026-05-04 14:53:36 +0800

    fix: PHPStan Level 8 strtotime type safety in OpenWeatherMapProvider

--- src/Providers/OpenWeatherMapProvider.php
+++ src/Providers/OpenWeatherMapProvider.php
@@ -239,7 +239,11 @@ class OpenWeatherMapProvider implements Provider
 
     protected function dateToWeek(string $date): string
     {
-        $week = date('N', strtotime($date));
+        $timestamp = strtotime($date);
+        if ($timestamp === false) {
+            return '';
+        }
+        $week = date('N', $timestamp);
         $map = ['1' => '周一', '2' => '周二', '3' => '周三', '4' => '周四', '5' => '周五', '6' => '周六', '7' => '周日'];
 
         return $map[$week];
